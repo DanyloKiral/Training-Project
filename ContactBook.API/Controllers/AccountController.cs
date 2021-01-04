164216b5b2f142e15fcbbdb777922e93f9fe49b7
VECTOR-SOFTWARE\tkopanskyi <tkopanskyi@vector-software.com>
2021-01-04 18:40:08 +0200

    Added AccountController, AccountRepository, DataService, User class

---
+++ ContactBook.API/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using ContactBook.API.Models;
+using ContactBook.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactBook.API.Controllers
+{
+    public class AccountController : Controller
+    {
+        public User LogIn(string email, string pass)
+        {
+            var user = DataService.GetUser(email, pass);
+            
+            return user;
+        }
+    }
+}
---
+++ ContactBook.API/Models/AccountRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactBook.API.Models
+{
+    public static class AccountRepository
+    {
+        public static List<User> Users;
+
+        static AccountRepository()
+        {
+            Users = new List<User>();
+            Users.AddRange( new[]
+            {
+                new User
+                {
+                    FullName = "John",
+                    Role = "admin",
+                    Email = "admin@mail.com",
+                    Password = "admin"
+                },
+                new User
+                {
+                    FullName = "Tom",
+                    Role = "user",
+                    Email = "user@mail.com",
+                    Password = "user"
+                }
+            });
+        }
+
+    }
+}

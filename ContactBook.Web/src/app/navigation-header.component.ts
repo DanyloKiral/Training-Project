2ef341404b7abcf04e1a933c4cbe7b780a743792
VECTOR-SOFTWARE\tkopanskyi <tkopanskyi@vector-software.com>
2021-01-27 19:22:26 +0200

    Added navigation header

---
+++ ContactBook.Web/src/app/navigation-header.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'cb-navigation-header',
+  templateUrl: 'navigation-header.component.html',
+  styleUrls: ['navigation-header.component.css'],
+})
+export class NavigationHeaderComponent {}

a765e898010a363e94a3909a7f46ce58bb8e88a4
VECTOR-SOFTWARE\tkopanskyi <tkopanskyi@vector-software.com>
2021-01-27 19:20:30 +0200

    Added necessary imports into app module

--- ContactBook.Web/src/app/app.module.ts
+++ ContactBook.Web/src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NavigationHeaderComponent} from './navigation-header.component';
+import { WelcomeComponent } from './welcome.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationHeaderComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:
+  [
+    AppComponent
+  ]
 })
 export class AppModule { }
